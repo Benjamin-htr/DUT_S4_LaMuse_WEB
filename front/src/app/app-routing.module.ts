16cfdacdfba82b68e53645587d45c81311aa872f
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2021-03-04 16:06:34 +0100

    ajout routes angular

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { EssaiComponent } from './essai/essai.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'weather', component: EssaiComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
