50a0a7c1da368825ce46e692e80c112f89665776
Loïc Domingues <loic.domingues@etudiant.univ-reims.fr>
2021-03-17 17:34:35 +0100

    Avancement Upload

--- front/src/app/la-muse-exec/la-muse-exec.component.ts
+++ front/src/app/la-muse-exec/la-muse-exec.component.ts
@@ -1,3 +1,4 @@
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { LaMuseCallService } from '../services/la-muse-call.service';
 
@@ -8,6 +9,7 @@ import { LaMuseCallService } from '../services/la-muse-call.service';
 })
 export class LaMuseExecComponent implements OnInit {
   title : string = 'Generate Images';
+  fileUploadService : any ;
 
 
   constructor(private rs : LaMuseCallService){}
@@ -34,8 +36,39 @@ export class LaMuseExecComponent implements OnInit {
   onFolderSelected(event){
     if (event.target.files.length > 0){
         let files = event.target.files;
-        console.log(files);
     }     
 }
 
+fileToUpload: File = null;
+handleFileInput(files: FileList) {
+  this.fileToUpload = files.item(0);
 }
+
+uploadFileToActivity() {
+  this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+    // do something, if upload success
+    }, error => {
+      console.log(error);
+    });
+}
+
+/*fichierAEnvoyer: File = null;
+
+//Fonction qui récupère le fichier pour l'ajouter à la variable
+//Elle est appelée lors d'un changement sur l'input du fichier
+//S'il y a plusieurs fichiers, il faudra adapter le code avec une variable de type "FileList" et parcourir la liste de fichiers avec une boucle.
+envoiFichier (fichiers: FileList) {
+    this.fichierAEnvoyer = fichiers.item(0);
+    console.log(fichiers.item(0));
+}
+*/
+//Fonction qui va lier l'attribut au service qui envoie le fichier au site ou à l'API. On utilise pour cela le système de souscription issue de la programmation réactive
+/*envoiFichierParLeService() {
+    this.envoiFichierService.postFile(this.fichierAEnvoyer).subscribe(resultat => {
+      // Code à développer ici en cas de succès de l'envoi du fichier
+      }, erreur => {
+        console.log("Erreur lors de l'envoi du fichier : ", erreur);
+      });*/
+}
+
+
